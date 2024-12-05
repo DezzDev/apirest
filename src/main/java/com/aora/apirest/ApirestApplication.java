577580902f222909484d00569b4910ea4065babf
Daniel Zapata <daniel-dezz@hotmail.com>
2024-12-05 12:15:37 +0100

    add dotenv dependency

--- src/main/java/com/aora/apirest/ApirestApplication.java
+++ src/main/java/com/aora/apirest/ApirestApplication.java
@@ -3,6 +3,9 @@ package com.aora.apirest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+
 
 @SpringBootApplication
 public class ApirestApplication {
@@ -10,7 +13,12 @@ public class ApirestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApirestApplication.class, args);
 		
-		
+		// Carga el archivo .env
+		Dotenv dotenv = Dotenv.configure().load();
+
+		// Opcional: Establece variables de entorno en el sistema (útil para `application.properties`)
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 
 	
 	}
